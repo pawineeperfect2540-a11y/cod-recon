8c898fe2e0e956e0be3dacbe94d6ec8f20b8d248
pawineeperfect2540-a11y <pawinee.perfect2540@gmail.com>
2026-07-16 14:23:38 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD3xP2lbqerKjzXeAaOCbKUJ8ospHCRSlw",
+  authDomain: "perfectads-deposit.firebaseapp.com",
+  projectId: "perfectads-deposit",
+  storageBucket: "perfectads-deposit.firebasestorage.app",
+  messagingSenderId: "825880055779",
+  appId: "1:825880055779:web:9c1d3dd57533bacbd69ad6",
+  measurementId: "G-95LTQMEE9T"
